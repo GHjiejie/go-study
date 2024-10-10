dfbf3fea5af63e01b711797b20418d41393e574b
zhengjie.vendor <zhengjie.vendor@sensetime.com>
2024-10-10 17:21:57 +0800

    test:bcrypt-demo

--- go-web-example/sessions/main.go
+++ go-web-example/sessions/main.go
@@ -49,6 +49,5 @@ func main() {
 	http.HandleFunc("/secret", secret)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
-
 	http.ListenAndServe(":8080", nil)
 }
---
+++ go-web-example/密码哈希值处理/go.mod
@@ -0,0 +1,5 @@
+module password-hash-demo
+
+go 1.22.6
+
+require golang.org/x/crypto v0.28.0 // indirect
